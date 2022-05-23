ced8d37f3f1ab6bdb7d2a902f83357de400f1137
Marko Sterk <marko_sterk@hotmail.com>
2022-05-23 21:02:25 +0200

    Disabled user registration.

--- app/routes/publicRoutes.py
+++ app/routes/publicRoutes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_jwt_extended import jwt_required
 from ..models.productModel import Product
 
 
@@ -7,12 +8,14 @@ publicRoutes = Blueprint('publicRoutes', __name__)
 
 
 @publicRoutes.route('/', methods=['GET'])
+@jwt_required()
 def index():
     products = Product.findMany({'quantity': {'$gt': 0}, 'active': True})
     return render_template('public/products.html', products = products)
 
 
 @publicRoutes.route('/products', methods=['GET'])
+@jwt_required()
 def products():
     products = Product.findMany({'quantity': {'$gt': 0}, 'active': True})
     return render_template('public/products.html', products = products)
